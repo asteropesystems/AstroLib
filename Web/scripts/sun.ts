409e5f423226d2c4c826b8446da53696f2bc2dbf
Garry <garry.pilkington.websites@gmail.com>
2019-06-22 16:24:39 +0100

    Added sun calculations and tests

--- Web/scripts/sun.ts
+++ Web/scripts/sun.ts
@@ -3,105 +3,187 @@
 
 'use strict';
 namespace AstroLib {
-  export class Sun {
-    getSunPosition = (date, lat, lng) => {
-        var pi = Math.PI;
-        var rad = pi / 180;
-        var lw = rad * -lng;
-        var phi = rad * lat;
+    export class Sun {
 
-        var dateConverter = new DateConverter();
-        var days = dateConverter.convertToDays(date);
+        rightAscension = (l, b) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var cos = Math.cos;
+            var tan = Math.tan;
+            var atan = Math.atan2;
+            var rad = pi / 180;
+            var e = rad * 23.4397;
 
-        var timeConverter = new TimeConverter();
-
-        var c = this.sunCoords(days);
-        var h = timeConverter.siderealTime(days, lw) - c.ra;
-
-        //return true;
-
-        return {
-            azimuth: this.azimuth(h, phi, c.dec),
-            altitude: this.altitude(h, phi, c.dec)
+            return atan(sin(l) * cos(e) - tan(b) * sin(e), cos(l));
         };
-        
-    };
 
-    azimuth = (h, phi, dec) =>{
-        var pi = Math.PI;
-        var sin = Math.sin;
-        var cos = Math.cos;
-        var tan = Math.tan;
-        var atan = Math.atan2;
+        declination = (l, b) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var cos = Math.cos;
+            var asin = Math.asin;
+            var rad = pi / 180;
+            var e = rad * 23.4397;
 
-        return atan(sin(h), cos(h) * sin(phi) - tan(dec) * cos(phi));
-    }
+            return asin(sin(b) * cos(e) + cos(b) * sin(e) * sin(l));
+        };
 
-    altitude = (h, phi, dec) =>{
-        var pi = Math.PI;
-        var rad = pi / 180;
-        var sin = Math.sin;
-        var cos = Math.cos;
-        var asin = Math.asin;
+        azimuth = (h, phi, dec) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var cos = Math.cos;
+            var tan = Math.tan;
+            var atan = Math.atan2;
 
-        return asin(sin(phi) * sin(dec) + cos(phi) * cos(dec) * cos(h));
-    }
+            return atan(sin(h), cos(h) * sin(phi) - tan(dec) * cos(phi));
+        }
+        altitude = (h, phi, dec) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var cos = Math.cos;
+            var asin = Math.asin;
 
-    sunCoords = (d) => {
-        var m = this.solarMeanAnomaly(d);
-        var l = this.eclipticLongitude(m);
+            return asin(sin(phi) * sin(dec) + cos(phi) * cos(dec) * cos(h));
+        }
 
-        return {
-            dec: this.declination(l, 0),
-            ra: this.rightAscension(l, 0)
+        siderealTime = (d, lw) => {
+            var pi = Math.PI;
+            var rad = pi / 180;
+
+            return rad * (280.16 + 360.9856235 * d) - lw;
+        }
+
+        solarMeanAnomaly = (d) => {
+            var pi = Math.PI;
+            var rad = pi / 180;
+
+            return rad * (357.5291 + 0.98560028 * d);
+        }
+
+        eclipticLongitude = (m) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var rad = pi / 180;
+            var c = rad * (1.9148 * sin(m) + 0.02 * sin(2 * m) + 0.0003 * sin(3 * m));
+            var p = rad * 102.9372;
+
+            return m + c + p + pi;
+        }
+
+        sunCoords = (d) => {
+            var m = this.solarMeanAnomaly(d);
+            var l = this.eclipticLongitude(m);
+
+            return {
+                dec: this.declination(l, 0),
+                ra: this.rightAscension(l, 0)
+            };
+        }
+
+        getSunPosition = (date, lat, lng) => {
+            var pi = Math.PI;
+            var rad = pi / 180;
+            var lw = rad * -lng;
+            var phi = rad * lat;
+
+            var dateConverter = new DateConverter();
+
+            var d = dateConverter.convertToDays(date);
+
+            var c = this.sunCoords(d);
+            var h = this.siderealTime(d, lw) - c.ra;
+
+            return {
+                azimuth: this.azimuth(h, phi, c.dec),
+                altitude: this.altitude(h, phi, c.dec)
+            };
+        };
+
+        julianCycle = (d, lw) => {
+            var pi = Math.PI;
+            var j0 = 0.0009;
+
+            return Math.round(d - j0 - lw / (2 * pi));
+        };
+
+        approxTransit = (ht, lw, n) => {
+            var pi = Math.PI;
+            var j0 = 0.0009;
+
+            return j0 + (ht + lw) / (2 * pi) + n;
+        };
+
+        solarTransitJ = (ds, m, l) => {
+            var sin = Math.sin;
+            var j2000 = 2451545;
+
+            return j2000 + ds + 0.0053 * sin(m) - 0.0069 * sin(2 * l);
+        };
+
+        hourAngle = (h, phi, d) => {
+            var pi = Math.PI;
+            var sin = Math.sin;
+            var cos = Math.cos;
+            var acos = Math.acos;
+
+            return acos((sin(h) - sin(phi) * sin(d)) / (cos(phi) * cos(d)));
+        };
+
+        getSetJ = (h, lw, phi, dec, n, m, l) => {
+            var w = this.hourAngle(h, phi, dec);
+            var a = this.approxTransit(w, lw, n);
+
+            return this.solarTransitJ(a, m, l);
+        };
+
+        getSunTimes = (date, lat, lng) => {
+            var pi = Math.PI;
+            var rad = pi / 180;
+            var lw = rad * -lng;
+            var phi = rad * lat;
+
+            var dateConverter = new DateConverter();
+
+            var d = dateConverter.convertToDays(date);
+            var n = this.julianCycle(d, lw);
+            var ds = this.approxTransit(0, lw, n);
+
+            var m = this.solarMeanAnomaly(ds);
+            var l = this.eclipticLongitude(m);
+            var dec = this.declination(l, 0);
+            var jnoon = this.solarTransitJ(ds, m, l);
+            var i;
+            var len;
+            var time;
+            var jset;
+            var jrise;
+            var dateConverter = new DateConverter();
+
+            var result = {
+                solarNoon: dateConverter.convertJulianDateToDate(jnoon),
+                nadir: dateConverter.convertJulianDateToDate(jnoon - 0.5)
+            };
+
+            var times = times = [
+                [-0.833, 'sunrise', 'sunset'],
+                [-0.3, 'sunriseEnd', 'sunsetStart'],
+                [-6, 'dawn', 'dusk'],
+                [-12, 'nauticalDawn', 'nauticalDusk'],
+                [-18, 'nightEnd', 'night'],
+                [6, 'goldenHourEnd', 'goldenHour']
+            ];
+
+            for (i = 0, len = times.length; i < len; i += 1) {
+                time = times[i];
+
+                jset = this.getSetJ(time[0] * rad, lw, phi, dec, n, m, l);
+                jrise = jnoon - (jset - jnoon);
+
+                result[time[1]] = dateConverter.convertJulianDateToDate(jrise);
+                result[time[2]] = dateConverter.convertJulianDateToDate(jset);
+            }
+
+            return result;
         };
     }
-
-    solarMeanAnomaly = (d) =>{
-        var pi = Math.PI;
-        var rad = pi / 180;
-
-        return rad * (357.5291 + 0.98560028 * d);
-    }
-
-    eclipticLongitude = (m) =>{
-        var pi = Math.PI;
-        var rad = pi / 180;
-        var sin = Math.sin;
-
-        var c = rad * (1.9148 * sin(m) + 0.02 * sin(2 * m) + 0.0003 * sin(3 * m));
-        var p = rad * 102.9372;
-
-        return m + c + p + pi;
-    }
-
-    declination = (l, b) =>{
-        var pi = Math.PI;
-        var rad = pi / 180;
-        var sin = Math.sin;
-        var cos = Math.cos;
-        var tan = Math.tan;
-        var atan = Math.atan2;
-
-        var e = rad * 23.4397;
-
-        return atan(sin(l) * cos(e) - tan(b) * sin(e), cos(l));
-    }
-
-    rightAscension = (l, b) =>{
-        var pi = Math.PI;
-        var rad = pi / 180;
-        var sin = Math.sin;
-        var cos = Math.cos;
-        var tan = Math.tan;
-        var atan = Math.atan2;
-
-        var e = rad * 23.4397;
-
-        return atan(sin(l) * cos(e) - tan(b) * sin(e), cos(l));
-    }
-
-
-  }
-
 }
--- Web/test/spec/sunSpec.ts
+++ Web/test/spec/sunSpec.ts
@@ -10,19 +10,47 @@ describe('In the file sun.ts', () =>{
     let lat;
     let long;
     let position;
+    let sunTimes;
 
     beforeEach(() =>{
         date = new Date("2019-06-19");
         lat = 45.00;
         long = 2.67;
-        position = { azimuth: -3.138708999166892, altitude: 0.7309219114456494 }
+        position = { azimuth: -3.102702486267067, altitude: -0.37629863789588613 }
+
+        sunTimes = {
+            "solarNoon": "2019-06-19T11:51:52.202Z",
+            "nadir": "2019-06-18T23:51:52.202Z",
+            "sunrise": "2019-06-19T04:03:28.630Z",
+            "sunset": "2019-06-19T19:40:15.773Z",
+            "sunriseEnd": "2019-06-19T04:07:09.178Z",
+            "sunsetStart": "2019-06-19T19:36:35.225Z",
+            "dawn": "2019-06-19T03:26:02.738Z",
+            "dusk": "2019-06-19T20:17:41.665Z",
+            "nauticalDawn": "2019-06-19T02:36:16.761Z",
+            "nauticalDusk": "2019-06-19T21:07:27.642Z",
+            "nightEnd": "2019-06-19T01:30:12.240Z",
+            "night": "2019-06-19T22:13:32.164Z",
+            "goldenHourEnd": "2019-06-19T04:48:43.968Z",
+            "goldenHour": "2019-06-19T18:55:00.435Z"
+          }
 
         sut = new AstroLib.Sun();
     })
 
     describe('getSunPosition', ()=>{
-        it('should be equal to 2458653.5', ()=>{
+        it('should have altitude= -0.37629863789588613 and azimuth= -3.102702486267067', ()=>{
             expect(sut.getSunPosition(date, lat, long)).toEqual(position);
+        })
+    })
+
+    describe('getSunTimes', ()=>{
+        it('should be equal to 2458653.5', ()=>{
+            var solarNoon = '2019-06-19T11:51:52.202Z';
+            var actual = sut.getSunTimes(date, lat, long);
+            var actualSolarNoon = actual.solarNoon.toISOString();
+
+            expect(actualSolarNoon).toEqual(solarNoon);
         })
     })
 
